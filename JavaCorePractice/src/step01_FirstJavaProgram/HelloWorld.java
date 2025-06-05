f41c33e184e3c5c95b047a19ff82cb3edfa33e03
Ramdas <ramdas.qualityfirst@gmail.com>
2025-06-05 16:34:04 +0530

    Step 1: First Java Program

---
+++ JavaCorePractice/src/step01_FirstJavaProgram/HelloWorld.java
@@ -0,0 +1,10 @@
+package step01_FirstJavaProgram;
+
+public class HelloWorld {
+
+	public static void main(String args[]) {
+
+		System.out.println("Hey, Ramdas Tejam here...");
+		System.out.println("Becoming better in java with daily practice.");
+	}
+}
