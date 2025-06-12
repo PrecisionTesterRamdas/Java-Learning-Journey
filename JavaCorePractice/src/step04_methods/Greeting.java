cabb525f45570dab7d8f5a02cfcd82e486a5385f
Ramdas <ramdas.qualityfirst@gmail.com>
2025-06-12 10:15:55 +0530

    Step 4.1: Created a simple method and called it using an object

---
+++ JavaCorePractice/src/step04_methods/Greeting.java
@@ -0,0 +1,13 @@
+package step04_methods;
+
+public class Greeting {
+
+	void sayHello() {
+		System.out.println("Hello, welcome to my java learning repo!");
+	}
+
+	public static void main(String[] args) {
+		Greeting obj = new Greeting();
+		obj.sayHello();
+	}
+}
