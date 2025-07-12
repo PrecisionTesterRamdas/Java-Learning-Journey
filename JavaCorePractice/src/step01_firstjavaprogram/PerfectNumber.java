5d374a0979f11321bb28b05055e4d8f56f49d6aa
Ramdas <ramdas.qualityfirst@gmail.com>
2025-07-13 00:40:30 +0530

    Step on 13/07/25 - Tried random problem for this day.
    > have solved the issue effectively and then enhanced the solution using
    AI suggestions.

---
+++ JavaCorePractice/src/step01_firstjavaprogram/PerfectNumber.java
@@ -0,0 +1,32 @@
+package step01_firstjavaprogram;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+
+	static int number;
+
+	public static void isPerfectNumber(int num) {
+		
+		int sum = 0;
+		for (int i = 1; i <= num/2; i++) {
+			if (num % i == 0) {
+				sum = sum + i;
+			}
+		}
+		if(num == sum) {
+			System.out.println("Number is perfect");
+		}
+		else {
+			System.out.println("Number is not perfect");
+		}
+	}
+
+	public static void main(String args[]) {
+
+		System.out.print("Enter a positive number: ");
+		Scanner sc = new Scanner(System.in);
+		number = sc.nextInt();
+		PerfectNumber.isPerfectNumber(number);
+	}
+}
